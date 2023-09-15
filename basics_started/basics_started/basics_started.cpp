1c7e6dacb3671103b2ae2c11f14fabb88af5b8e2
SuperSection <soumosarkar297@gmail.com>
2023-09-16 00:32:26 +0530

    conditional & ternary

--- basics_started/basics_started/basics_started.cpp
+++ basics_started/basics_started/basics_started.cpp
@@ -30,6 +30,7 @@ int main() {
 */
 
 
+/*
 #include <iostream>
 #include <cstdint>
 
@@ -48,6 +49,36 @@ int main() {
 	fun = 0b111111110; // binary number representation
 	fun = 0x1FE;	   // hexadecimal number representation
 	printf("fun value is %d\n", fun);
+
+	return 0;
+}
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int rating=3;
+
+	if (rating == 5) {
+		puts("5 star rated");
+	}
+	else if (rating == 4) {
+		puts("4 star rated");
+	}
+	else {
+		puts("NOT 5 star rated");
+	}
+
+	if (true) { 
+		// it gets executed if the condition(between parentheses) is true
+		puts("Go for it");
+	}
+
+	// conditional statement using ternary operator
+	printf("Your rating feedback is %s\n", rating < 4 ? "true block" : "false block");
 
 	return 0;
 }
