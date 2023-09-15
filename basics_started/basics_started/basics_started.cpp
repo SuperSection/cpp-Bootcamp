058fcb9aec742a5bf5994c1449146f069a5efebe
SuperSection <soumosarkar297@gmail.com>
2023-09-16 00:49:01 +0530

    conditional as switch

--- basics_started/basics_started/basics_started.cpp
+++ basics_started/basics_started/basics_started.cpp
@@ -55,6 +55,7 @@ int main() {
 */
 
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -79,6 +80,39 @@ int main() {
 
 	// conditional statement using ternary operator
 	printf("Your rating feedback is %s\n", rating < 4 ? "true block" : "false block");
+
+	return 0;
+}
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int rating = 50;
+
+	switch (rating) {
+		case 1:
+			puts("Rated as 1 star");
+			break;
+		case 2:
+			puts("Rated as 2 star");
+			break;
+		case 3:
+			puts("Rated as 3 star");
+			break;
+		case 4:
+			puts("Rated as 4 star");
+			break;
+		case 5:
+			puts("Rated as 5 star");
+			break;
+		default:
+			puts("Please rate out app only between 1 and 5");
+			break;
+	}
 
 	return 0;
 }
